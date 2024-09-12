c82ec5660fb5bb71059442805b79ffa6fcff1038
Waleed Alhrbi <waleedalhrbi2001@gmail.com>
2024-09-12 13:36:06 +0300

    Creeat img

---
+++ main.js
@@ -0,0 +1,56 @@
+let btn1 = document.getElementById("one");
+let btn2 = document.getElementById("two");
+let btn3 = document.getElementById("three");
+let btn4 = document.getElementById("four");
+let btn5 = document.getElementById("five");
+let btn6 = document.getElementById("six");
+let btn7 = document.getElementById('seven');
+let btn8 = document.getElementById('eight');
+let btn9 = document.getElementById('nine');
+
+
+let tvimg = document.getElementById('tvimg');
+btn1.addEventListener('click', function(){
+    tvimg.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg";
+    tvimg.style.display = "block";
+});
+
+btn2.addEventListener('click', function(){
+    tvimg.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg";
+    tvimg.style.display = "block";
+});
+
+btn3.addEventListener('click', function(){
+    tvimg.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg";
+    tvimg.style.display = "block";
+});
+
+btn4.addEventListener('click', function(){
+    tvimg.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg";
+    tvimg.style.display = "block";
+});
+
+btn5.addEventListener('click', function(){
+    tvimg.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg";
+    tvimg.style.display = "block";
+});
+
+btn6.addEventListener('click', function(){
+    tvimg.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg";
+    tvimg.style.display = "block";
+});
+
+btn7.addEventListener('click', function(){
+    tvimg.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg";
+    tvimg.style.display = "block";
+});
+
+btn8.addEventListener('click', function(){
+    tvimg.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg";
+    tvimg.style.display = "block";
+});
+
+btn9.addEventListener('click', function(){
+    tvimg.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg";
+    tvimg.style.display = "block";
+});
